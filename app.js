9484f86583da647b35bd853cbd4549ef9bd50150
AndreaPotenza <andreapoten3@gmail.com>
2024-11-29 17:43:20 +0100

    completo

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const postsRouter = require('./routers/posts')
+const errorsHandler = require('./middleware/errorsHandler')
+const notFound = require('./middleware/notFound')
 // const postsList = require('./posts')
 
 app.use(express.json())
@@ -15,6 +17,9 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.send('il server del blog')
 })
+
+app.use(errorsHandler)
+app.use(notFound)
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
